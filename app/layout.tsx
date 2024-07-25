ac84743fe00e1259a46cd485f0bb89a7e67dacab
“jakhongirav” <jakhongir.av@gmail.com>
2024-07-26 00:07:55 +0500

    main page changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,22 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Jile L",
+    template: "%s | jakhongir.av",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Software engineer",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
-    images: [
-      {
-        url: "https://chronark.com/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
+    title: "jakhongir.av",
+    description: "Software engineer",
+    url: "https://jakhongirav.vercel.app",
+    siteName: "jakhongir.av",
+    // images: [
+    //   {
+    //     url: "https://chronark.com/og.png",
+    //     width: 1920,
+    //     height: 1080,
+    //   },
+    // ],
     locale: "en-US",
     type: "website",
   },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Jakhongirav",
     card: "summary_large_image",
   },
   icons: {
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
