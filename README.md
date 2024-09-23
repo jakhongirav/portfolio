a91e24c83062a0c6f8bb92c1859332a2a0d297dd
“jakhongirav” <jakhongir.av@gmail.com>
2024-09-23 23:01:01 +0500

    fixing vulnerabilities

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 <div align="center">
     <a href="https://chronark.com"><h1 align="center">jakhongirav.life</h1></a>
 
-<<<<<<< HEAD
 My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com?ref=chronark.com), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
 
-=======
-My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/) and deployed to [Vercel](https://vercel.com/).
->>>>>>> 82c230d0055e257dc7479710576a05d33b789f50
 </div>
 
 <br/>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import "../global.css";
-import { Inter } from "@next/font/google";
-import LocalFont from "@next/font/local";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
+// Metadata for SEO and social sharing
 export const metadata: Metadata = {
   title: {
     default: "Jile L",
     template: "%s | jakhongirav",
   },
   description: "Software engineer",
+  metadataBase: new URL("https://jakhongirav.life"), // Set the base URL here
   openGraph: {
-    title: "jakhongir.av",
+    title: "jakhongirav",
     description: "Software engineer",
     url: "https://jakhongirav.life",
     siteName: "jakhongirav",
@@ -33,12 +35,14 @@ export const metadata: Metadata = {
     shortcut: "/favicon.png",
   },
 };
+
+// Using the built-in next/font for Google and local fonts
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-const calSans = LocalFont({
+const calSans = localFont({
   src: "../public/fonts/CalSans-SemiBold.ttf",
   variable: "--font-calsans",
 });
