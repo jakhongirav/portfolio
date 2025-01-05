deb9019d5fdc47f29dcb12dbbb2a27175e089683
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-01-05 07:43:13 +0100

    Changing resume URL

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const navigation = [
   { name: "Contact", href: "/contact" },
   {
     name: "Resume",
-    href: "https://drive.google.com/file/d/1HN_uxErQIBaYyZ_rWlzWuQ0czucFOaL6/view?usp=sharing",
+    href: "https://drive.google.com/file/d/1C890wv1D_bJu5S7-nnHjzxYnYYhD9Ds9/view?usp=drive_link",
   },
 ];
 
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
+        quantity={1000}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Jakhongir
